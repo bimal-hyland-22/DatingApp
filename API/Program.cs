2a6e534d6c737f756796839c1c8c88775d8d6ace
Bimal Paul <Bimal.Paul@hyland.com>
2023-09-16 19:00:45 +0530

    Completed section 3

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<DataContext>(opt=>{
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -24,9 +25,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+
 
 app.MapControllers();
 
